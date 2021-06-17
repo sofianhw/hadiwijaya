7de642f14adda7af6492ff3eb809317188aea3c2
sofianhw <sofian@warungpintar.co>
2021-06-17 23:22:00 +0700

    change to my name

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,21 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>SlipiconValley</title>
+        <title>Sofian Hadiwijaya`</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="keywords" content="bukit algoritma, silicon valley, teknologi, teknopolis, slipicon valley" />
-        <meta name="description" content="Asal muasal banyak ekosistem perusahaan rintisan (startup), tempat bertemunya Industri, Investor, dan Inventor"/>
+        <meta name="keywords" content="sofianhw, sofian, hadiwijaya, sofian hadiwijaya" />
+        <meta name="description" content="Sofian Hadiwijaya atau biasa dikenal sofianhw, adalah seorang konsultan dibidang teknologi dan perusahaan rintisan"/>
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://slipiconvalley.com">SlipiconValley!</a>
+          Hi! My name is <br />
+          Sofian <a href="https://hadiwijaya.co.id">Hadiwijaya!</a>
         </h1>
 
         <p className={styles.description}>
-          If you landing on this page and have some Ideas, <br />
-          Please reach me on <a href="https://www.linkedin.com/in/sofianhw/">Linkedin</a> or <a href="https://www.instagram.com/sofianhw/">Instagram</a>
+          If you have some Ideas <br />
+          Please reach me on <a href="https://www.linkedin.com/in/sofianhw/">Linkedin</a>
         </p>
       </main>
       <footer className={styles.footer}>
